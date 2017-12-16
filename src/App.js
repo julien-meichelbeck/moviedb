e6c25a289092ad99b7c2983cf4eabab27b8c85cb
Julien Meichelbeck <julien.meichelbeck@gmail.com>
2017-12-16 12:58:50 +0100

    Autocomplete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react'
-import recompact from './recompact'
-import logo from './logo.svg'
+import React from 'react'
+import recompact from 'recompact'
+import { BehaviorSubject } from 'rxjs'
+import axios from 'axios'
 import './App.css'
 
-export default recompact.withProps({ a: 1 })(({ a }) => (
+const API_KEY = 'd533369a57d21cba80e1b1c060585c47'
+const url = 'https://api.themoviedb.org/3/movie/550?api_key=d533369a57d21cba80e1b1c060585c47'
+
+export default recompact.compose(
+  recompact.withObs({
+    text$: new BehaviorSubject(''),
+  }),
+  recompact.connectObs(({ text$ }) => ({
+    onChange: text$,
+    value: text$,
+    results: text$
+      .distinctUntilChanged()
+      .switchMap(query =>
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&language=fr`)
+      )
+      .switchMap(response => response.json())
+      .pluck('results'),
+  })),
+  recompact.withHandlers({
+    onChange: ({ onChange }) => event => onChange(event.target.value),
+  })
+)(({ value, onChange, results }) => (
   <div className="App">
-    <p>{a}</p>
+    <textarea value={value} onChange={onChange} />
+    <ul>{results && results.map(({ title }) => <li key={title}>{title}</li>)}</ul>
   </div>
 ))
